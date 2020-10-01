175eaa3b6566dda2588b781a56aa7f367c594d4c
Akash Gupta <akashgzb98@gmail.com>
2020-10-02 00:04:37 +0530

    Build Data addedgit status!

--- src/components/UI/Icons/Github.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Github from '../../../assets/IconGithub.png';
-import classes from './Icon.css';
-
-const IconGithub = ()=> <img src={Github} alt="Github" className={classes.Icon} />;
-
-export default IconGithub;
--- src/components/UI/Icons/Linkedin.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Linkedin from '../../../assets/IconLinkedin.png';
-import classes from './Icon.css';
-
-const IconLinkedin = ()=> <img src={Linkedin} alt="Fb Link" className={classes.Icon} />;
-
-export default IconLinkedin;
